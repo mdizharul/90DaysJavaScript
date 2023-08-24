2ecd37c55b8aff24a049b256e0825dfadaee761e
mdizharul <mdizharul.ansari@genzeon.com>
2023-08-24 14:57:57 +0530

    object commit

---
+++ Objects.js
@@ -0,0 +1,21 @@
+//Basic Example: we are drawing diffrent kind of shapes(circle, square, etc)
+
+// Objects are the collection of key-value pairs
+
+// OOP style of programming
+
+const circle={
+  radius : 1,
+  coOrdinates : {
+    x :1, 
+    y :1
+  },
+  isVisible : true,
+  draw : function(){
+    console.log("Drawing circle");
+  }
+};
+
+circle.draw();
+
+
