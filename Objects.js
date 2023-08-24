83ab6bfd03a155780e6d0fb0238398a8478fcd61
mdizharul <mdizharul.ansari@genzeon.com>
2023-08-24 15:19:56 +0530

    second commit

--- Objects.js
+++ Objects.js
@@ -10,17 +10,44 @@
 
 // Factory Functions
 // we create a new function called createCircle and we move the code into it.
-function createCircle(){
-  const circle = {
-    radius : 1,
-    coOrdinates : {
-      x :1, 
-      y :1
-    },
-    isVisible : true,
-    draw : function(){
+// function createCircle(){
+//   const circle = {
+//     radius : 1,
+//     coOrdinates : {
+//       x :1, 
+//       y :1
+//     },
+//     isVisible : true,
+//     draw : function(){
+//       console.log("Drawing circle");
+//     }
+//   };
+  
+// }
+
+
+// improving the code & removing fields to understanding and simplicity
+// function createCircle(radius){
+//   return {
+//     radius : radius, // if key and values are same  we can skip value
+//     draw : function(){
+//       console.log("Drawing circle");
+//     }
+//   };
+  
+// }
+
+function createCircle(radius){
+  return {
+    radius : radius, // if key and values are same  we can skip value
+    draw() {
       console.log("Drawing circle");
     }
-  };
-  
+  }; 
 }
+
+const circle1=createCircle(7);
+console.log(circle1);
+
+const circle2=createCircle(14);
+console.log(circle2);
